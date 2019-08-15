940a806982fc9c70f85813ad1d54a5bce6bf5551
Zeljko Jagust <admin@bot-informatika.hr>
2019-08-15 11:55:30 +0200

    Debian 10 Buster initial customization script

--- debian-initial-customization.sh
+++ debian-initial-customization.sh
@@ -24,40 +24,40 @@
 
 function initialise ()
 {
-    trap ctrlC SIGINT   # Trap Ctrl+C.
+	trap ctrlC SIGINT   # Trap Ctrl+C.
 
-    # Misc items
-    declare -gr SPACER='----------------------------------------------------------------------------------------------------'
+	# Misc items
+	declare -gr SPACER='----------------------------------------------------------------------------------------------------'
 	declare -gr E=$'\e[1;31;103m'			# (E) Error: highlighted text.
 	declare -gr W=$'\e[1;31;103m'			# (W) Warning: highlighted text.
 	declare -gr B=$'\e[1m'				# B for Bold.
 	declare -gr R=$'\e[0m'				# R for Reset.
 
-    # Display a warning.
+	# Display a warning.
 	clear
 
 	# Show a warning.
 	cat <<-END
+ 
+		${SPACER}
 
-	${SPACER}
+		${B}WARNING${R}
 
-	${B}WARNING${R}
+		This script is only to be run on Debian 10 minimal installations, as described on the following link:
+		https://zacks.eu/debian-buster-minimal-installation/
 
-	This script is only to be run on Debian 10 minimal installations, as described on the following link:
-    https://zacks.eu/debian-buster-minimal-installation/
+		The script has a specific purpose and runs a certain tasks involving operating system environment
+		customization and software installation. Running this script anywhere at any time will leave you
+		with potentially un-bootable OS and software you may not want.
 
-    The script has a specific purpose and runs a certain tasks involving operating system environment
-    customization and software installation. Running this script anywhere at any time will leave you
-    with potentially un-bootable OS and software you may not want.
+		A detailed overview of tasks this script will perform can be seen on the following link:
+		https://zacks.eu/debian-10-buster-initial-customization/
 
-    A detailed overview of tasks this script will perform can be seen on the following link:
-    https://zacks.eu/debian-10-buster-initial-customization/
-
-    Please make sure you understand what is written above!
-
+		Please make sure you understand what is written above!
+ 
 	END
 
-    # Ask for confirmation.
+	# Ask for confirmation.
 	local ANSWER
 	read -rp "Type ${B}Y${R} to proceed, or anything else to cancel, and press Enter: ${B}" ANSWER
 	echo "${R}"
